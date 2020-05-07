3b69606acce46aaeb7ff0dd2f3ac178a7e7656ac
j.avellaneda.galindo <j.avellaneda.galindo@accenture.com>
2020-05-06 19:21:11 -0500

    Version alfa chat

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,21 +1,30 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
 
+var sala = params.get('sala');
+
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+    $('small').text(sala);
+
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log(resp);
+        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -25,28 +34,23 @@ socket.on('disconnect', function() {
 
 });
 
-
-// Enviar información
-/* socket.emit('crearMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-}); */
-
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
     console.log(personas);
+    renderizarUsuarios(personas);
 });
 
-// Mensajes privador
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado:', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
